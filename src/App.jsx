80bae1cc1242a765c9b02f13bbea94ecb9927ee2
chesterblack <chez.black@hotmail.co.uk>
2023-08-18 23:58:19 +0100

    Blocked access to non-DMs

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [sceneReady, setSceneReady] = useState(false);
-
+  const [isDM, setIsDM] = useState(false);
+  
   useEffect(() => {
     OBR.scene.isReady().then(setSceneReady);
     return OBR.scene.onReadyChange(setSceneReady);
+  }, []);
+
+  useEffect(() => {
+    console.log('checking role')
+    OBR.player.getRole().then((role) => {
+      console.log(role);
+      setIsDM(role === "GM");
+    });
   }, []);
 
   useEffect(() => {
@@ -17,12 +26,19 @@ function App() {
   }, []);
 
   if (sceneReady) {
-    return <HealthTracker />;
+    if (isDM) {
+      return <HealthTracker />;
+    } else {
+      return (
+        <>
+          <h1>Health Tracker</h1>
+          <p>Sorry, only the DM can see this...</p>
+        </>
+      );
+    }
   } else {
     // Show a basic header when the scene isn't ready
-    return (
-      <>Just a mo...</>
-    );
+    return <h1>Just a mo...</h1>;
   }
 }
 
